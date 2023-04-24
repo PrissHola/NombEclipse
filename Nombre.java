6569bca9f6b0eaa1578ef2135b56c6bad86d8707
PrissHola <campospriscila479@gmail.com>
2023-04-24 01:10:26 -0600

    se agregó un comentario

--- Nombre.java
+++ Nombre.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Nombre {
-
+//holaaa
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
--- Nombre2.java
+++ Nombre2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Nombre2 {
-
+//holaaaa
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
